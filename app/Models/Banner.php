2f6cccc0713559a664ed357b1c4cc9696aae5ebd
omar546565 <omar546565@gmail.com>
2022-01-31 20:46:45 +0300

    asd696

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Banner extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name', 'old_price', 'price', 'created_at', 'updated_at', 'discount', 'description', 'images', 'image', 'in_favorites', 'in_cart'
+        'image', 'category', 'product'
     ];
 
 
